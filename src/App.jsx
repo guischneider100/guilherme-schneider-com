9ee4b0c008af1f27437209c0ab68cf5f975ac719
guischneider100 <guilhermeschneider@unochapeco.edu.br>
2025-07-09 18:00:32 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img  src="src/assets/pixel-art.png" alt="Profile" className="profile-photo" />
+      </div>
+      <h1>
+        Guilherme Felipe Schneider
+        <span className="cursor">_</span>
+      </h1>
+      <h2>Full Stack Developer</h2>
+    </>
+  )
+}
+
+export default App
